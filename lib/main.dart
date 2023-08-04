1c05d8cd69ec583fc9df6e9326857c914e101955
bhavikbhuva <bhavik.wrteam@icloud.com>
2023-08-04 15:01:19 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> {
       create: (context)=>CounterProvider(),
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Counter using Provider',
+        title: 'Counter using Provider ',
         home: HomePage(),
       ),
     );
